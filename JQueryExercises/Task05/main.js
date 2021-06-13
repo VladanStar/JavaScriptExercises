c4cdee190d13eb0344c47c28d5039d023247fb4d
VladanStar <vladancupric@gmail.com>
2021-06-13 23:47:57 +0200

    New commit

---
+++ JQueryExercises/Task05/main.js
@@ -0,0 +1,7 @@
+var btn = $('#btn');
+btn.on('click', function () {
+    // alert($('#naslov').text());
+    // alert($('input').val());
+    // console.log($('.dd').html())
+    console.log($('#naslov').css('font-size'));
+})
