91b7c841a931469ad0f1b08b24083ca65b1ce870
unknown <vladancupric@gmail.com>
2021-06-14 12:34:49 +0200

    New commit

--- JQueryExercises/Task06/main.js
+++ JQueryExercises/Task06/main.js
@@ -1,7 +1,12 @@
-$d(document).ready(function(){
+// $d(document).ready(function(){
 
-
-$('button').on('click', function(){
-    alert('1')
+// $('button').on('click', function(){
+//     alert('1')
+// })
+// _})
+// $(function () {
+//   alert(1);
+// });
+jQuery(document).ready(function($){
+alert("Vladan Cupric");
 })
-_})
