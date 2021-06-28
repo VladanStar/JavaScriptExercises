d705f303aaeaf78e1d08b3edda27148d050cc0bd
unknown <vladancupric@gmail.com>
2021-06-28 09:33:11 +0200

    New commit JSON

---
+++ AJAX/Task07/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load',main);
+function main(){
+  let str = '[{"a":10.1, "b":3.12},{"a":223, "b":1.12},{"a":-7.0, "b":0.01},{"a":7.4, "b":12.9}]'
+
+let niz = JSON.parse(str);
+
+}
