4a6c6991856790975f5b93c89406c2ccb3d44dda
unknown <vladancupric@gmail.com>
2021-06-30 08:33:49 +0200

    New commits

--- AJAX/Task07/main.js
+++ AJAX/Task07/main.js
@@ -1,7 +1,29 @@
 window.addEventListener('load',main);
+
+function obradaElementa(el){
+return el.a + el.b;
+}
 function main(){
   let str = '[{"a":10.1, "b":3.12},{"a":223, "b":1.12},{"a":-7.0, "b":0.01},{"a":7.4, "b":12.9}]'
 
 let niz = JSON.parse(str);
+let minimum = Number.MAX_VALUE;
+let maximum = Number.MIN_VALUE;
+let suma = 0;
+
+for(let el of niz){
+    let rez = obradaElementa(el);
+    if(rez < minumum){
+        minimum = rez;
+    }
+    if (rez >maximum){
+        maximum =rez;
+    }
+    suma += rez;
+}
+let average = suma/niz.length;
+document.getElementById('minimum').innerHTML = minimun;
+document.getElementById('average').innerHTML = average;
+document.getElementById('maximum').innerHTML = maximum
 
 }
---
+++ AJAX/Task08/main.js
@@ -0,0 +1,23 @@
+var thePets = [
+  {
+    "name": "Meowsalat",
+    "species": "cat",
+    "favFood": "tuna",
+  },
+  {
+    
+    "name": "Barky",
+    "species": "dog",
+    "favFood": "meatlof",
+  },
+  {
+    "name": "flufy",
+    "species": "lion",
+    "favFood": "pork",
+  },
+  {
+    "name": "Leonida",
+    "species": "pig",
+    "favFood": "trash",
+  },
+];
