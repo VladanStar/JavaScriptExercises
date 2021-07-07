95a48c93900cfcdfe49672ec206aa66d85444f50
unknown <vladancupric@gmail.com>
2021-07-07 13:11:45 +0200

    New commits

---
+++ AJAXJSON/Task20/js/main.js
@@ -0,0 +1,12 @@
+
+// var x = jQuery('ul>:first-child');
+// x.addClass('myclass');
+// $('ul').find('a').eq(0).css('color','gold');
+
+// function name(){
+//     console.log('hello');
+// }
+// var x = name;
+// x();
+
+$('li').addClass('myClass');
