e243ec356e30a8e0de9dbd2b7373f98cceb8bde3
unknown <vladancupric@gmail.com>
2021-08-25 10:55:55 +0200

    Net Ninja

--- ReactNetNinjaExercises/task01/src/components/BlogList/BlogList.js
+++ ReactNetNinjaExercises/task01/src/components/BlogList/BlogList.js
@@ -1,21 +1,27 @@
 import React from "react";
 
+const BlogList = ({ blogs, title, handleDelete }) => {
 
-const BlogList = ({blogs,title})=>{
-    return (
-        <div className="blog-list">
+   
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
 
-            <h2>{title}</h2>
-
-             {blogs.map(blog => (
+      {/* {blogs.map(blog => (
 
         <div className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
           </div>
-          ))}
+          ))} */}
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <button onClick={()=>handleDelete(blog.id)}>Delete Blog</button>
         </div>
-
-    );
-}
+      ))}
+    </div>
+  );
+};
 export default BlogList;
--- ReactNetNinjaExercises/task01/src/components/Home/Home.js
+++ ReactNetNinjaExercises/task01/src/components/Home/Home.js
@@ -1,20 +1,42 @@
-import { useState } from "react";
-import BlogList from "../BlogList/BlogList"
+import { useState, useEffect } from "react";
+import BlogList from "../BlogList/BlogList";
 
 const Home = () => {
+
+  const handleDelete =(id)=>{
+        const newBlogs = blogs.filter(blog=>blog.id !== id);
+        setBlogs(newBlogs)
+
+  }
+
+  useEffect(()=>{
+    console.log('use effect run');
+    console.log(blogs);
+  },[]);
   const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  const[name, setName] = useState('mario')
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
-        </div>
-  
- 
+      <BlogList blogs={blogs} title="All Blogs" handleDelete ={handleDelete} />
+      
+      <button onClick={()=>setName('luigi')}>SetName</button>
+      <p>{name}</p>
+      {/* <BlogList
+        blogs={blogs.filter((blog) => blog.author === "mario")}
+        title="Mario's blogs"
+      /> */}
+    </div>
   );
-}
- 
+};
+
 export default Home;
