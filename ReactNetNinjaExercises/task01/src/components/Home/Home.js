c3675be6f30a2b70c598d78b1122699170b8a8d7
unknown <vladancupric@gmail.com>
2021-08-27 21:45:02 +0200

    New commits

--- ReactNetNinjaExercises/task01/src/components/Home/Home.js
+++ ReactNetNinjaExercises/task01/src/components/Home/Home.js
@@ -1,24 +1,17 @@
 import { useState, useEffect } from "react";
 import BlogList from "../BlogList/BlogList";
+import useFetch from "../UseFetch/UseFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  useEffect(()=>{
-    fetch('http://localhost:8000/blogs')
-    .then(res=>{
-     return res.json()
-    })
-    .then(data=>{
-      setBlogs(data)
-    })
-  },[]);
-
-
+const {error, isPedding, data:blogs} = useFetch("http://localhost:8000/blogs");
+  
+  
   // const[name, setName] = useState('mario')
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPedding && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs}  />}
       
       
