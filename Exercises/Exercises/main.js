1adec68af75db4da72e48c7a39a4bddbaf839310
unknown <vladancupric@gmail.com>
2021-11-28 21:22:00 +0100

    New commits

---
+++ Exercises/Exercises/main.js
@@ -0,0 +1,6 @@
+function  addElement() {
+    var h1 = document.createElement('H1');
+    var text = document.createTextNode("Hello World");
+    h1.appendChild(text);
+    document.body.appendChild(h1);
+}
