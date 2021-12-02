cbf403d7e312ab8f495a61409019d4113329fd86
VladanStar <vladancupric@gmail.com>
2021-12-02 21:26:43 +0100

    New commits

--- Exercises/exercises03.js
+++ Exercises/exercises03.js
@@ -1,7 +1,23 @@
+//1.
 var a = 123;
 function f() {
-    console.log(a);
-    var a = 1;
-    console.log(a)
+  console.log(a);
+  var a = 1;
+  console.log(a);
 }
 f();
+//2.
+var global = 1;
+function f() {
+  var local = 2;
+  global++;
+  return global;
+}
+console.log(f());
+//3.
+function f() {
+  local = 2;
+  return local * 2;
+}
+f();
+console.log(f());
