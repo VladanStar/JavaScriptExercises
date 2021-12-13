0b17288f7c5c2057476b48be6ed8fa4cdfffe65f
unknown <vladancupric@gmail.com>
2021-12-13 15:49:13 +0100

    New commits

--- WorkWebExample/js/main.js
+++ WorkWebExample/js/main.js
@@ -1,15 +1,16 @@
 
 
-const searchStringelement = document.querySelector('.searchString');
+const searchStringElement = document.querySelector('.searchString');
 const dgmPretragaElement = document.querySelector('.dgmPretraga');
 const prikazElement = document.querySelector('.prikaz');
 
 dgmPretragaElement.addEventListener('click', ()=>{
-    
-});
+    fetch(`https://www.omdbapi.com/?apikey=a68ef358&s=${searchStringElement.value}`)
+    .then(res => res.json())
+    .then((data)=>{
+        console.log(data);
+    })
 
 
 
-{/* <div class="col">
-            Column
-          </div> */}
+})
