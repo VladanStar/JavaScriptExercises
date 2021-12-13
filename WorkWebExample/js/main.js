6fdc2f38594b7baa56f3c6181e6de6a27f347ff5
unknown <vladancupric@gmail.com>
2021-12-13 14:54:29 +0100

    New commits

---
+++ WorkWebExample/js/main.js
@@ -0,0 +1,15 @@
+
+
+const searchStringelement = document.querySelector('.searchString');
+const dgmPretragaElement = document.querySelector('.dgmPretraga');
+const prikazElement = document.querySelector('.prikaz');
+
+dgmPretragaElement.addEventListener('click', ()=>{
+    
+});
+
+
+
+{/* <div class="col">
+            Column
+          </div> */}
