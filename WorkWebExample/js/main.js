9d360051e15bb8f73d0855c4cf9560e8df1f6211
unknown <vladancupric@gmail.com>
2021-12-13 16:26:34 +0100

    New commits

--- WorkWebExample/js/main.js
+++ WorkWebExample/js/main.js
@@ -2,6 +2,11 @@ const searchStringElement = document.querySelector(".searchString");
 const dgmPretragaElement = document.querySelector(".dgmPretraga");
 const prikazElement = document.querySelector(".prikaz");
 
+let film = [];
+const setFilm = (list) => {
+  film = [...list];
+};
+
 dgmPretragaElement.addEventListener("click", () => {
   fetch(
     `https://www.omdbapi.com/?apikey=a68ef358&s=${searchStringElement.value}`
@@ -9,9 +14,17 @@ dgmPretragaElement.addEventListener("click", () => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data.Search);
+      //   film = data.Search;
+      setFilm(data.Search);
+      render();
+    })
+    .catch((error) => console.log(error));
+});
 
-      data.Search.forEach((element) => {
-        prikazElement.innerHTML += `
+const render = () => {
+    prikazElement.innerHTML = '';
+  film.forEach((element) => {
+    prikazElement.innerHTML += `
             
             
             <div class="col">
@@ -21,11 +34,10 @@ dgmPretragaElement.addEventListener("click", () => {
             <div class="card-body">
                 <h5 class="card-title">${element.Title}</h5>
                 <p class="card-text">Tip: ${element.Type} Godina: ${element.Year}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary">Dodaj u korpu</a>
             </div>
             </div>
             </div>
             `;
-      });
-    });
-});
+  });
+};
