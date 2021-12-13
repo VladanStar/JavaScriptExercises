d43de34487b4bee6589a4ed299c71971a2f336a5
unknown <vladancupric@gmail.com>
2021-12-13 16:58:22 +0100

    New commits

--- WorkWebExample/js/main.js
+++ WorkWebExample/js/main.js
@@ -1,7 +1,7 @@
 const searchStringElement = document.querySelector(".searchString");
 const dgmPretragaElement = document.querySelector(".dgmPretraga");
 const prikazElement = document.querySelector(".prikaz");
-const prikazOdabranih = document.querySelector('.prikazOdabranih');
+const prikazOdabranihElement = document.querySelector('.prikazOdabranih');
 
 let film = [];
 let odabraniFilmovi = [];
@@ -23,6 +23,7 @@ dgmPretragaElement.addEventListener("click", () => {
       //   film = data.Search;
       setFilm(data.Search);
       render();
+     
     })
     .catch((error) => console.log(error));
 });
@@ -53,4 +54,38 @@ const render = () => {
 const dodaj = (idx) =>{
     setOdabraniFilmovi([...odabraniFilmovi, film[idx]]);
     console.log(odabraniFilmovi);
+    renderOdabrani();
 }
+
+const brisanje = (idx) =>{
+    let temp= [...odabraniFilmovi];
+    temp.splice(idx,1);
+    setOdabraniFilmovi(temp);
+    console.log(odabraniFilmovi);
+    renderOdabrani();
+}
+
+
+const renderOdabrani = () => {
+    prikazOdabranihElement.innerHTML = '';
+
+
+  odabraniFilmovi.forEach((element, idx) => {
+    prikazOdabranihElement.innerHTML += `
+            
+            
+            <div class="col">
+            
+            <div class="card" style="width: 18rem;">
+            <img src="${element.Poster}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${element.Title}</h5>
+                <p class="card-text">Tip: ${element.Type} Godina: ${element.Year}</p>
+                <a href="#" class="btn btn-primary" onClick = "brisanje(${idx})">Obrisi</a>
+            </div>
+            </div>
+            </div>
+            `;
+  });
+};
+
