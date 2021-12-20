ea85f54d54768188c894cee7012a4f0323aac8ee
unknown <vladancupric@gmail.com>
2021-12-20 23:46:55 +0100

    New commits

--- Exercises/Exercises03/exercises02.js
+++ Exercises/Exercises03/exercises02.js
@@ -54,3 +54,84 @@ console.log([1,2,3]
 //37
 
 //-------7.------------
+
+class NumberBox {
+  constructor(initialValue){
+    this._value = initialValue;
+  }
+  get value(){
+    return this._value;
+  }
+  set value(newValue){
+    this._value = newValue
+  }
+  get increment(){
+    this._value
+  }
+}
+const box = new NumberBox(2);
+box.increment;
+console.log(box.value);
+//2
+
+//------- 8.-------
+const kobe = {
+  points:21,
+  score:()=>{
+    this.points++;
+    this.points++;
+    this.points++;
+  }
+}
+kobe.score();
+kobe.score();
+kobe.score();
+console.log(kobe.points);
+//21
+
+
+//--------- 9. -------------
+const arr= [1,2,3];
+const mappedArr = arr.map(i=>i **2);
+
+let data ={};
+data[arr]=arr;
+data[mappedArr] =mappedArr;
+console.log(data.arr, data[mappedArr]);
+//undefined [ 1, 4, 9 ]
+
+
+//---------10.----------
+const basicData = {id:"333-222"};
+const relatedId="wow";
+
+let data={};
+data[basicData] =basicData;
+data[relatedId]=relatedId;
+console.log(data);
+//{ '[object Object]': { id: '333-222' }, wow: 'wow' }
+
+//------------11.----------
+const data = {a:3,b:4};
+Object.freeze(data);
+const otherData= {b:5,c:6};
+Object.seal(otherData);
+
+const completeData= Object.assign({}, data, otherData);
+console.log(completeData.b);
+//5
+
+//--------- 12 ----------
+function sum(a,b,c){
+  let i =c;
+  let res=0;
+  for(let i=0; i<c;i++){
+    res +=arguments[i];
+  }
+  res += arguments[i]
+  return res;
+}
+console.log(sum(1,2,'3'));
+//33undefined
+
+//-----------13.--------
