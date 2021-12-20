6106ac9f6e71737bf5c590bada69b8dd985db3e9
unknown <vladancupric@gmail.com>
2021-12-20 20:02:39 +0100

    New commits

--- Exercises/Exercises03/exercises02.js
+++ Exercises/Exercises03/exercises02.js
@@ -28,3 +28,29 @@ const then = async ()=>{
     console.log(await Promise.resolve(3))
 };
 then()
+
+//----------4 -------------
+Promise.resolve(3)
+.then(()=>Promise.resolve(4))
+.then(x=>console.log(x));
+//  4
+
+//-----------5.-------
+const personOne = {
+  name:"Boban",
+  age:30
+};
+let boban=personOne;
+boban.age=28;
+console.log("boban.age");
+// boban,age
+
+//-------6.-----------
+console.log([1,2,3]
+.map(x=> x ** 3)
+.filter(x=> x>1)
+.map(x=> x+1)
+.reduce((a,b)=>a+b,0));
+//37
+
+//-------7.------------
