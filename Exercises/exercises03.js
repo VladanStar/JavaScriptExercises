7e00e7193c5ec932d62bf83d4dc5533811a4c828
unknown <vladancupric@gmail.com>
2022-01-04 22:35:49 +0100

    New commits

--- Exercises/exercises03.js
+++ Exercises/exercises03.js
@@ -256,3 +256,27 @@ mojeIme = 'Marko';
 
 console.log(x);
 let x = 5;
+
+console.log(x);
+var x = 5;
+
+console.log(x);
+x = 5;
+
+console.log("buducnost vraće:", buducnost());
+
+function buducnost() {
+    return "Još uvek ne postoje leteći automobili";
+};
+
+
+
+var test = function (){
+  console.log("prikaz iz funkcijskog izraza");
+}
+
+function test(){
+  console.log("prikaz iz deklarisane funkcije");
+}
+
+test();  
