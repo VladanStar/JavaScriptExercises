ca02c46124bb0979ced9d571425a18d4462770bf
unknown <vladancupric@gmail.com>
2022-01-04 21:49:23 +0100

    New commits

--- Exercises/exercises03.js
+++ Exercises/exercises03.js
@@ -222,3 +222,27 @@ let omotacZa2 = omotajVrednost(2);
 
 console.log(omotacZa1());
 console.log(omotacZa2());
+
+function uvecajBrojac() {
+  let brojac = 0;
+  return function () {
+      return brojac++;
+  };
+}
+
+const izbroj = uvecajBrojac();
+
+console.log(izbroj());
+console.log(izbroj());
+console.log(izbroj());
+
+let uvecaj = () => {
+  let brojac = 0;
+  return () => brojac++;
+}
+
+const izbroj2 = uvecaj();
+
+console.log(izbroj2());
+console.log(izbroj2());
+console.log(izbroj2());
