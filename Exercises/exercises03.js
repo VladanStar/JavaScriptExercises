23d8eda931a66d64cf179b6781926df21ad9bad3
VladanStar <vladancupric@gmail.com>
2022-01-06 14:31:24 +0100

    New commits

--- Exercises/exercises03.js
+++ Exercises/exercises03.js
@@ -362,3 +362,31 @@ x = 6 * Math.random()-3
 console.log(x);
 x = 6 * Math.random()-3
 console.log(x);
+
+function bezArgumenata() {}
+
+// Ovo je OK
+bezArgumenata(1, 2, 3);
+
+function triArgumenta(a, b, c) {
+  console.log("---\n" + a);
+  console.log(b);
+  console.log(c);
+}
+
+// I ovo je OK
+triArgumenta(1, 2, "tri");
+triArgumenta(1, 2);
+triArgumenta(1);
+triArgumenta();
+
+function triArgumentaPodrazumenvano(a = "a", b = "b", c = null) {
+  console.log("---\n" + a);
+  console.log(b);
+  console.log(c);
+}
+
+triArgumentaPodrazumenvano(1, 2, "tri");
+triArgumentaPodrazumenvano(1, 2);
+triArgumentaPodrazumenvano(1);
+triArgumentaPodrazumenvano();
