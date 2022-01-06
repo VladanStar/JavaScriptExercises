132edbabbddbbc586952f2e56fbe8009e208f9ee
VladanStar <vladancupric@gmail.com>
2022-01-06 15:26:40 +0100

    New commits

--- Exercises/exercises03.js
+++ Exercises/exercises03.js
@@ -402,3 +402,27 @@ console.log(test);
 //globalna vrednost
 //lokalna vrednost
 //lokalna vrednost
+
+function uvecajBrojac() {
+  let brojac = 0;
+  return function () {
+    return brojac++;
+  };
+}
+
+const izbroj = uvecajBrojac();
+
+console.log(izbroj());
+console.log(izbroj());
+console.log(izbroj());
+
+let uvecaj = () => {
+  let brojac = 0;
+  return () => brojac++;
+};
+
+const izbroj2 = uvecaj();
+
+console.log(izbroj2());
+console.log(izbroj2());
+console.log(izbroj2());
