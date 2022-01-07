77e51d71ca768b6ba364f0ea3d05b7ece561f348
VladanStar <vladancupric@gmail.com>
2022-01-07 09:20:33 +0100

    New commits

--- Exercises/exercises03.js
+++ Exercises/exercises03.js
@@ -494,3 +494,14 @@ console.log(nizBrojeva[1 - 1]);
 
 console.log(nizBrojeva[17 - 1]);
 console.log(nizBrojeva[-1]);
+
+let niz = [1, 3, "Mika", "pera", false];
+
+console.log("---");
+for (let i = 0; i < niz.length; i++) console.log(niz[i]);
+
+console.log("---");
+for (let i in niz) console.log(niz[i]);
+
+console.log("---");
+for (let x of niz) console.log(x);
