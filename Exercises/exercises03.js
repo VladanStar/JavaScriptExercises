ffe8eac83bc228ea17900ea1a2ecacf3988d1094
VladanStar <vladancupric@gmail.com>
2022-01-07 09:35:17 +0100

    New commits

--- Exercises/exercises03.js
+++ Exercises/exercises03.js
@@ -505,3 +505,26 @@ for (let i in niz) console.log(niz[i]);
 
 console.log("---");
 for (let x of niz) console.log(x);
+
+var podsetnik = [];
+
+function podsetiMe(zadatak) {
+  podsetnik.push(zadatak);
+}
+
+function staJeSledece() {
+  return podsetnik.shift();
+}
+
+function hitnoMePodseti(zadatak) {
+  podsetnik.unshift(zadatak);
+}
+
+podsetiMe("priprema slajdova za predavanja");
+podsetiMe("priprema zadataka");
+hitnoMePodseti("odgovoriti na pisma");
+podsetiMe("kupovina knjige");
+
+while (podsetnik.length != 0) {
+  console.log(staJeSledece());
+}
