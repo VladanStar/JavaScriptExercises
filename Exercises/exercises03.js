1bf7b26309060b34af1d167423c6e14428ec7585
unknown <vladancupric@gmail.com>
2022-01-11 09:11:52 +0100

    New commits

--- Exercises/exercises03.js
+++ Exercises/exercises03.js
@@ -623,3 +623,24 @@ const vrednost = function () {
       getPodatak: getPodatak
   };
 }();
+
+
+const Dogadjaj = require('events');
+
+class EmiterDogadjaja extends Dogadjaj {}
+
+const emiter = new EmiterDogadjaja();
+
+emiter.on('dogadjaj', function(a, b) {
+  console.log(`--- rukovalac dogadjajem je funkcija ---`);
+  console.log(a, b, this, this === emiter);
+});
+
+emiter.on('dogadjaj', (a, b) =>{
+  console.log(`--- rukovalac dogadjajem je lambda-izraz ---`);
+  console.log(a, b, this, this === emiter);
+});
+
+emiter.emit('dogadjaj', 'a', 6);
+emiter.emit('dogadjaj', 'mika');
+emiter.emit('dogadjaj');
