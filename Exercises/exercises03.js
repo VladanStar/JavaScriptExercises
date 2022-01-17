e16d5cffdffdf8bb3c1d060bfcdc1930b2508c46
unknown <vladancupric@gmail.com>
2022-01-17 13:37:16 +0100

    New commits

--- Exercises/exercises03.js
+++ Exercises/exercises03.js
@@ -958,3 +958,47 @@ console.log(niz.filter((a) => a>=0).reduce((a,b) =>a+b,0 ));
 
 console.log(niz.reduce((a,b) =>(a>b)?a:b));
 console.log(niz.reduce((a,b) =>(a<b)?a:b));
+
+function prikaziNisku(niska) {
+  return new Promise((razresi, odbij) => {
+      setTimeout(
+          () => {
+              console.log(niska);
+              razresi();
+          },
+          Math.floor(Math.random() * 50) + 1
+      );
+  });
+}
+
+function prikaziSve() {
+  prikaziNisku("A");
+  prikaziNisku("Б");
+  prikaziNisku("В");
+  prikaziNisku("Г");
+  prikaziNisku("Д");
+}
+
+prikaziSve();
+
+function prikaziNisku(niska) {
+  return new Promise((razresi, odbij) => {
+      setTimeout(
+          () => {
+              console.log(niska);
+              razresi();
+          },
+          Math.floor(Math.random() * 50) + 1
+      );
+  });
+}
+
+async function prikaziDvaRedom() {
+  await prikaziNisku("А");
+  await prikaziNisku("Б");
+  prikaziNisku("В");
+  prikaziNisku("Г");
+  prikaziNisku("Д");
+}
+
+prikaziDvaRedom();
