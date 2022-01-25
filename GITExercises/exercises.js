bd129be522a0a5c368bf98cfe205b09de6b258df
unknown <vladancupric@gmail.com>
2022-01-25 14:39:46 +0100

    New commits

--- GITExercises/exercises.js
+++ GITExercises/exercises.js
@@ -1,5 +1,213 @@
 
 
+
+
+// 3. What is the output of below code
+function main(){
+   console.log('A');
+   setTimeout(
+      function print(){ console.log('B'); }
+   ,0);
+   console.log('C');
+}
+main();
+// 1: A, B and C
+// 2: B, A and C
+// 3: A and C
+// 4: A, C and B
+// Answer
+// Answer: 4
+// The statements order is based on the event loop mechanism. The order of statements follows the below order,
+
+// At first, the main function is pushed to the stack.
+// Then the browser pushes the fist statement of the main function( i.e, A's console.log) to the stack, executing and popping out immediately.
+// But setTimeout statement moved to Browser API to apply the delay for callback.
+// In the meantime, C's console.log added to stack, executed and popped out.
+// The callback of setTimeout moved from Browser API to message queue.
+// The main function popped out from stack because there are no statements to execute
+// The callback moved from message queue to the stack since the stack is empty.
+// The console.log for B is added to the stack and display on the console.
+
+// 4. What is the output of below equality check
+console.log(0.1 + 0.2 === 0.3);
+// 1: false
+// 2: true
+// Answer
+// Answer: 1
+// This is due to the float point math problem. Since the floating point numbers are encoded in binary format, the addition operations on them lead to rounding errors. Hence, the comparison of floating points doesn't give expected results. You can find more details about the explanation here 0.30000000000000004.com/
+
+// 5. What is the output of below code
+var y = 1;
+  if (function f(){}) {
+    y += typeof f;
+  }
+  console.log(y);
+// 1: 1function
+// 2: 1object
+// 3: ReferenceError
+// 4: 1undefined
+// Answer
+// Answer: 4
+// The main points in the above code snippets are,
+
+// You can see function expression instead function declaration inside if statement. So it always returns true.
+// Since it is not declared(or assigned) anywhere, f is undefined and typeof f is undefined too.
+// In other words, it is same as
+
+var y = 1;
+  if ('foo') {
+    y += typeof f;
+  }
+  console.log(y);
+// Note: It returns 1object for MS Edge browser
+
+// 6. What is the output of below code
+function foo() {
+  return
+  {
+    message: "Hello World"
+  };
+}
+console.log(foo());
+// 1: Hello World
+// 2: Object {message: "Hello World"}
+// 3: Undefined
+// 4: SyntaxError
+// Answer
+// Answer: 3
+// This is a semicolon issue. Normally semicolons are optional in JavaScript. So if there are any statements(in this case, return) missing semicolon, it is automatically inserted immediately. Hence, the function returned as undefined.
+
+// Whereas if the opening curly brace is along with the return keyword then the function is going to be returned as expected.
+
+function foo() {
+  return {
+    message: "Hello World"
+  };
+}
+console.log(foo()); // {message: "Hello World"}
+
+// 7. What is the output of below code
+var myChars = ['a', 'b', 'c', 'd']
+delete myChars[0];
+console.log(myChars);
+console.log(myChars[0]);
+console.log(myChars.length);
+// 1: [empty, 'b', 'c', 'd'], empty, 3
+// 2: [null, 'b', 'c', 'd'], empty, 3
+// 3: [empty, 'b', 'c', 'd'], undefined, 4
+// 4: [null, 'b', 'c', 'd'], undefined, 4
+// Answer
+// Answer: 3
+// The delete operator will delete the object property but it will not reindex the array or change its length. So the number or elements or length of the array won't be changed. If you try to print myChars then you can observe that it doesn't set an undefined value, rather the property is removed from the array. The newer versions of Chrome use empty instead of undefined to make the difference a bit clearer.
+
+// 8. What is the output of below code in latest Chrome
+var array1 = new Array(3);
+console.log(array1);
+
+var array2 = [];
+array2[2] = 100;
+console.log(array2);
+
+var array3 = [,,,];
+console.log(array3);
+// 1: [undefined × 3], [undefined × 2, 100], [undefined × 3]
+// 2: [empty × 3], [empty × 2, 100], [empty × 3]
+// 3: [null × 3], [null × 2, 100], [null × 3]
+// 4: [], [100], []
+// Answer
+// Answer: 2
+// The latest chrome versions display sparse array(they are filled with holes) using this empty x n notation. Whereas the older versions have undefined x n notation. Note: The latest version of FF displays n empty slots notation.
+
+// 9. What is the output of below code
+const obj = {
+  prop1: function() { return 0 },
+  prop2() { return 1 },
+  ['prop' + 3]() { return 2 }
+}
+
+console.log(obj.prop1());
+console.log(obj.prop2());
+console.log(obj.prop3());
+// 1: 0, 1, 2
+// 2: 0, { return 1 }, 2
+// 3: 0, { return 1 }, { return 2 }
+// 4: 0, 1, undefined
+// Answer
+// Answer: 1
+// ES6 provides method definitions and property shorthands for objects. So both prop2 and prop3 are treated as regular function values.
+
+// 10. What is the output of below code
+console.log(1 < 2 < 3);
+console.log(3 > 2 > 1);
+// 1: true, true
+// 2: true, false
+// 3: SyntaxError, SyntaxError,
+// 4: false, false
+// Answer
+// Answer: 2
+// The important point is that if the statement contains the same operators(e.g, < or >) then it can be evaluated from left to right. The first statement follows the below order,
+
+console.log(1 < 2 < 3);
+console.log(true < 3);
+console.log(1 < 3); // True converted as 1 during comparison
+// True
+// Whereas the second statement follows the below order,
+
+console.log(3 > 2 > 1);
+console.log(true > 1);
+console.log(1 > 1); // False converted as 0 during comparison
+// False
+
+// 11. What is the output of below code in non-strict mode
+function printNumbers(first, second, first) {
+  console.log(first, second, first);
+}
+printNumbers(1, 2, 3);
+// 1: 1, 2, 3
+// 2: 3, 2, 3
+// 3: SyntaxError: Duplicate parameter name not allowed in this context
+// 4: 1, 2, 1
+// Answer
+// Answer: 2
+// In non-strict mode, the regular JavaScript functions allow duplicate named parameters. The above code snippet has duplicate parameters on 1st and 3rd parameters. The value of the first parameter is mapped to the third argument which is passed to the function. Hence, the 3rd argument overrides the first parameter.
+
+// Note: In strict mode, duplicate parameters will throw a Syntax Error.
+
+// 12. What is the output of below code
+const printNumbersArrow = (first, second, first) => {
+  console.log(first, second, first);
+}
+printNumbersArrow(1, 2, 3);
+// 1: 1, 2, 3
+// 2: 3, 2, 3
+// 3: SyntaxError: Duplicate parameter name not allowed in this context
+// 4: 1, 2, 1
+// Answer
+// Answer: 3
+// Unlike regular functions, the arrow functions doesn't not allow duplicate parameters in either strict or non-strict mode. So you can see SyntaxError in the console.
+
+// 13. What is the output of below code
+const arrowFunc = () => arguments.length;
+console.log(arrowFunc(1, 2, 3));
+// 1: ReferenceError: arguments is not defined
+// 2: 3
+// 3: undefined
+// 4: null
+// Answer
+// Answer: 1
+// Arrow functions do not have an arguments, super, this, or new.target bindings. So any reference to arguments variable tries to resolve to a binding in a lexically enclosing environment. In this case, the arguments variable is not defined outside of the arrow function. Hence, you will receive a reference error.
+
+// Where as the normal function provides the number of arguments passed to the function
+
+const func = function () {
+                    return arguments.length;
+                    }
+console.log(func(1, 2, 3));
+// But If you still want to use an arrow function then rest operator on arguments provides the expected arguments
+
+const arrowFunc = (...args) => args.length;
+console.log(arrowFunc(1, 2, 3));
+
 // 14. What is the output of below code
 console.log( String.prototype.trimLeft.name === 'trimLeft' );
 console.log( String.prototype.trimLeft.name === 'trimStart' );
