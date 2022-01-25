ce2bd26f6c00b45f1e715ae91a58cdbd57cd0bf6
unknown <vladancupric@gmail.com>
2022-01-25 17:37:02 +0100

    New commits

--- GITExercises/exercises.js
+++ GITExercises/exercises.js
@@ -388,6 +388,7 @@ console.log(func());
 // 2: 10
 // 3: SyntaxError
 // 4: Promise {<resolved>: undefined}
+//Promise { <pending> }
 // Answer
 // Answer: 4
 // The await expression returns value 10 with promise resolution and the code after each await expression can be treated as existing in a .then callback. In this case, there is no return expression at the end of the function. Hence, the default return value of undefined is returned as the resolution of the promise. The above async function is equivalent to below expression,
