129715a20aa6b51245c4d882d259b78e8fd6180d
unknown <vladancupric@gmail.com>
2022-02-02 21:58:12 +0100

    New commits

--- Teletrader/JavaScript/exercises.js
+++ Teletrader/JavaScript/exercises.js
@@ -1,5 +1,101 @@
 
-//12.
+// 1. 
+
+
+//2.
+
+function fun1(x){
+    x.firstName = "John";
+}
+let x = {
+    firstName:"Peter",
+    lastName:"Smith"
+}
+fun1(x);
+console.log(`${x.firstName} ${x.lastName}`)
+
+//John Smith
+//3. 
+
+for(var i=0; i<=5; i++){
+    setTimeout(function test(){
+        console.log(i);
+    }, i*1000)
+}
+
+//nakon jedne sekune  6 6 6 6 6 
+
+//4. 
+
+function first(){
+    var a =2;
+  second();
+    function second(){
+        console.log(a)
+    }
+}
+first();
+
+//2
+
+//5. 
+var myArr = [1,2,3,4,5];
+myArr.forEach(function(item){})
+console.log(myArr)
+
+
+//6.
+
+for(var i= 0; i<5; i++){
+    console.log(i);
+}
+console.log(i);
+
+//1 2 3 4 5
+
+//7.
+
+let A = {
+    name:'Object A',
+    update: function(){
+        let updateObjectA = function(){
+            this.name ='updated Object A';
+        }
+        updateObjectA()
+    }
+}
+A.update();
+console.log(A.name);
+console.log(this.name);
+
+// Object A
+// undefined
+
+//8.
+
+(function(x=3){
+    return (function(y=4){
+        let a =10;
+        console.log(x);
+    })(2);
+})(1);
+
+//1
+
+//9.
+
+let x =4;
+function func(x){
+    x+=7;
+    --x;
+    function func2(x=2){
+        x=x*4;
+    }
+}
+func(x);
+console.log(x);
+
+//4
 
 
 //13.
@@ -10,12 +106,14 @@ var test = function(){
     var x = 20;
 }
 test();
+
 //undefined
 
 //14.
 
 var niz = [1, "test",true];
 console.log(niz+12);
+
 //1,test,true12
 
 //15.
@@ -26,18 +124,24 @@ delete niz[2];
 niz.forEach((item,i )=>{
     console.log(item + " " + i)
 })
+
 // 1 0
 // 2 1
 // 4 3
 // 5 4
+
 //16.
+
 for(var i=1; i<=5;i++){
     setTimeout(function timer(){
         console.log(i);
     },i*1000)
 }
+
 // 6 6 6 6 6 
+
 //17.
+
 const a = [1,2,3];
 const func1 = ()=>{
      a[10] =99;
@@ -47,9 +151,11 @@ function print(){
     console.log(a[6]);
 }
 func1();
+
 //undefined
 
 //19.
+
 const func1 =(x,y=null) =>{
     xy(y)
 }
@@ -60,10 +166,12 @@ function xy(){
     var counter2 = 2;
 }
 func1();
+
 //undefined
 //undefined
 
 //20.
+
  (function(){
     console.log("A");
     setTimeout(function(){console.log("B");},1000);
@@ -72,4 +180,5 @@ func1();
 
     
 })();
+
 //A D C B
