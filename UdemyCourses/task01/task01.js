3243c8991c2f830bb63ca35fb0cddc5fa1eceaca
unknown <vladancupric@gmail.com>
2022-03-03 14:05:57 +0100

    New commits

---
+++ UdemyCourses/task01/task01.js
@@ -0,0 +1,18 @@
+class Human {
+    gender ='Male';
+    printGender = ()=>{
+        console.log(this.gender);
+    }
+}
+class Person extends Human {
+    name = "Max";
+    gender = 'female';
+
+   printMyName = ()=> {
+       console.log(this.name);
+   }
+}
+
+const person = new Person();
+person.printMyName();
+person.printGender();
