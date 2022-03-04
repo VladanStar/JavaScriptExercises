1319e78d4ebc8de64cbed83f07d05208a5fe89bf
unknown <vladancupric@gmail.com>
2022-03-04 09:30:53 +0100

    New commits

--- UdemyCourses/first-project/src/App.js
+++ UdemyCourses/first-project/src/App.js
@@ -1,9 +1,11 @@
-
+import React from 'react';
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
+      <ExpenseItem />
       <h1>Lets go started</h1>
       <h2>This is also visible!</h2>
 
