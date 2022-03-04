f01f8041b05950d24b76221187c280faf47f91c2
unknown <vladancupric@gmail.com>
2022-03-04 11:45:37 +0100

    New commits

--- UdemyCourses/first-project/src/App.js
+++ UdemyCourses/first-project/src/App.js
@@ -5,9 +5,10 @@ import ExpenseItem from './components/ExpenseItem';
 function App() {
   return (
     <div className="App">
-      <ExpenseItem />
+      
       <h1>Lets go started</h1>
       <h2>This is also visible!</h2>
+      <ExpenseItem />
 
     </div>
   );
