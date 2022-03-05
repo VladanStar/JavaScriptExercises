c28fc4e5284885d13a4b0573f75b98de24a50c97
unknown <vladancupric@gmail.com>
2022-03-05 11:01:12 +0100

    New commits

--- UdemyCourses/first-project/src/components/ExpenseItem.js
+++ UdemyCourses/first-project/src/components/ExpenseItem.js
@@ -2,11 +2,18 @@ import React from "react";
 import "./ExpenseItem.css"
 
 const ExpenseItem = (props) => {
-   
+  const month =props.date.toLocaleString('en-US',{month:'long'})
+   const day = props.date.toLocaleString('en-US',{day:'2-digit'})
+   const year = props.date.getFullYear()
   return (
     < div className ="expense-item" >
       {/* <div>{props.id}</div> */}
-       <div>{props.date.toISOString()}</div> 
+       {/* <div>{props.date.toISOString()}</div>  */}
+       <div>
+         <div>{month}</div>
+         <div>{year}</div>
+         <div>{day}</div>
+       </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       
