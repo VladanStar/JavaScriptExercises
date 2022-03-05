b76886ce1abc6590d6dd5945b0c4b1f1d151e863
unknown <vladancupric@gmail.com>
2022-03-05 08:36:01 +0100

    New commits

--- UdemyCourses/first-project/src/components/ExpenseItem.js
+++ UdemyCourses/first-project/src/components/ExpenseItem.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./ExpenseItem.css"
 
-const ExpenseItem = () => {
-    const expenseDate  =new Date(2021, 1, 12);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 294.67;
+const ExpenseItem = (props) => {
+   
   return (
     < div className ="expense-item" >
-       <div>{expenseDate.toISOString()}</div> 
+      <div>{props.id}</div>
+       <div>{props.data}</div> 
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       
-      <div className="expense-item__price">${expenseAmount}</div>
+      <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
