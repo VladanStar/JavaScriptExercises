dc63e23a4799cfb698dafe28a62ae8261317cbc5
unknown <vladancupric@gmail.com>
2022-03-05 11:50:26 +0100

    New commits

--- UdemyCourses/first-project/src/components/ExpenseItem.js
+++ UdemyCourses/first-project/src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./ExpenseItem.css"
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
  
   return (
     < div className ="expense-item" >
+      <ExpenseDate />
      
       <div className="expense-item__description">
         <h2>{props.title}</h2>
