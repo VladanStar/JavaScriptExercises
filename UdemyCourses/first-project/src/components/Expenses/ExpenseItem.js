3f815b57d8b6767df50f2dce63dbd1cb47626e68
unknown <vladancupric@gmail.com>
2022-03-06 15:27:17 +0100

    New commits

--- UdemyCourses/first-project/src/components/Expenses/ExpenseItem.js
+++ UdemyCourses/first-project/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate"
 import Card from "../UI/Card"
 
 const ExpenseItem = (props) => {
+
+  useState();
+
+  let title = props.title;
+
+
+  const clickHandler = ()=>{
+ 
+    title = "Vladan is Great";
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
 
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button>Change Title</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
