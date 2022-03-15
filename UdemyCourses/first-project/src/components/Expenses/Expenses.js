1471314dbc4c01144bbd09d4fd754ec26a07e39a
VladanStar <vladancupric@gmail.com>
2022-03-15 23:30:00 +0100

    New commits

--- UdemyCourses/first-project/src/components/Expenses/Expenses.js
+++ UdemyCourses/first-project/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -13,24 +14,14 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString()=== filteredYear
   })
-  let expenseContent = <p>No Expenses Found</p>;
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
+ 
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      { expenseContent}
+      <ExpensesList items={ filteredExpenses}/>
     </Card>
   );
 };
