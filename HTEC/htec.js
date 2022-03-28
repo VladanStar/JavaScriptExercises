7873ba36455133f88834bdc861893f4223ea1c38
VladanStar <vladancupric@gmail.com>
2022-03-28 11:48:59 +0200

    New

--- HTEC/htec.js
+++ HTEC/htec.js
@@ -116,3 +116,100 @@ console.log(five == fiveOneMoreTime);
 console.log(five === fiveOneMoreTime);
 console.log(five == fiveString);
 console.log(five === fiveString);
+//18.
+var first = [1, 2, 3, 4, 5];
+var second = [1, 2, 3, 4, 5];
+console.log(first == second);
+console.log(first === second);
+
+//18.
+const person = {
+    name: "Vladan",
+    age: 49,
+    address: {
+        city: "Kragujevac",
+        street: "Kopaonicka",
+        number: 48
+    }
+}
+const secondPerson = { ...person };
+secondPerson.address.number = 7;
+console.log(person.address.number, secondPerson.address.number);
+
+//19.
+const arr = [1, 2, 3];
+const mapArr = arr.map(i => i ** 2);
+
+let data = {};
+data[arr] = arr;
+data[mapArr] = mapArr;
+console.log(data.arr, data[mapArr]); 
+
+//20.
+
+const basicData = { id: "333-222" };
+const relatedId = "wow";
+let data = {};
+data[basicData] = basicData;
+data[relatedId] = relatedId;
+
+//21.
+const kobe = {
+    points: 21,
+    score: () => {
+        this.points++;
+        this.points++;
+        this.points++;
+    }
+};
+kobe.score();
+kobe.score();
+kobe.score();
+console.log(kobe.points);
+
+//22.
+const getItemData = () =>( {
+    name: "Desk",
+        
+})
+console.log(getItemData().name);
+
+//23.
+const later = (fn, ...args) => fn(...args);
+console.log(later(x=>x,3))
+
+
+//24.
+
+const data = { a: 3, b: 4};
+Object.freeze(data);
+const otherData = { c: 5, d: 6 };
+Object.seal(otherData);
+const completeData = Object.assign({}, data, otherData);
+console.log(completeData.b)
+
+//25.
+
+// const arr = []
+// arr.x = 3;
+
+//26.
+
+class NumberBox {
+    constructor(initialValue) {
+        this._value = initialValue;
+    }
+    get value() {
+        return this._value;
+    }
+    set value(newValue) {
+        this._value = newValue;
+    }
+    get increment() {
+        this.value++;
+    }
+
+}
+const box = new NumberBox(2);
+box.increment;
+console.log(box.value)
