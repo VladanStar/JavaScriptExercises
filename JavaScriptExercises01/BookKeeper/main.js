62e270d73dd46456c78e91ab2d2af17730bbca20
unknown <vladancupric@gmail.com>
2022-08-01 09:12:17 +0200

    Add showModal function button

--- JavaScriptExercises01/BookKeeper/main.js
+++ JavaScriptExercises01/BookKeeper/main.js
@@ -14,3 +14,8 @@ function showModal() {
   modal.classList.add('show-modal');
   websiteNameEl.focus();
 }
+
+// Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
