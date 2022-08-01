d4c3f0672cc8aa70db6825254da6627de8a2670a
unknown <vladancupric@gmail.com>
2022-08-01 09:40:32 +0200

    Validate form

--- JavaScriptExercises01/BookKeeper/main.js
+++ JavaScriptExercises01/BookKeeper/main.js
@@ -19,3 +19,19 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+// Validate Form
+function validate(nameValue, urlValue) {
+    const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if (!nameValue || !urlValue) {
+      alert('Please submit values for both fields.');
+      return false;
+    }
+    if (!urlValue.match(regex)) {
+      alert('Please provide a valid web address.');
+      return false;
+    }
+    // Valid
+    return true;
+  }
