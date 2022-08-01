4f69eed983b091cc9b4c66620568c13c319fc3d6
unknown <vladancupric@gmail.com>
2022-08-01 09:11:18 +0200

    Add showModal function

--- JavaScriptExercises01/BookKeeper/main.js
+++ JavaScriptExercises01/BookKeeper/main.js
@@ -5,3 +5,12 @@ const bookmarkForm = document.getElementById('bookmark-form');
 const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
+
+
+let bookmarks = [];
+
+// Show Modal, Focus on Input
+function showModal() {
+  modal.classList.add('show-modal');
+  websiteNameEl.focus();
+}
