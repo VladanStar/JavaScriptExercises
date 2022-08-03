e53af62db5b85fad2d7db1023dc33207ca2ddc70
unknown <vladancupric@gmail.com>
2022-08-03 13:15:44 +0200

    New commits

--- JavaScriptExercises01/AnimatedNavigation/main.js
+++ JavaScriptExercises01/AnimatedNavigation/main.js
@@ -12,3 +12,21 @@ menuBars.addEventListener('click', toggleNav);
 navItems.forEach((nav) => {
   nav.addEventListener('click', toggleNav);
 });
+
+function toggleNav() {
+    // Toggle: Menu Bars Open/Closed
+    menuBars.classList.toggle('change');
+    // Toggle: Menu Active
+    overlay.classList.toggle('overlay-active');
+    if (overlay.classList.contains('overlay-active')) {
+      // Animate In - Overlay
+      overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
+      // Animate In - Nav Items
+      navAnimation('out', 'in');
+    } else {
+      // Animate Out - Overlay
+      overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
+      // Animate Out - Nav Items
+      navAnimation('in', 'out');
+    }
+  }
