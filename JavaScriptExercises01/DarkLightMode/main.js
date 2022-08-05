e1f6300001391f2303dfe9eedd65b6f815439004
unknown <vladancupric@gmail.com>
2022-08-05 16:13:06 +0200

    add selector

---
+++ JavaScriptExercises01/DarkLightMode/main.js
@@ -0,0 +1,7 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
