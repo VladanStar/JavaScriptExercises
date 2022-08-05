c6a770ed7566567e54eb42ff206673c7fcf62b75
unknown <vladancupric@gmail.com>
2022-08-05 16:24:20 +0200

    New commits

--- JavaScriptExercises01/DarkLightMode/main.js
+++ JavaScriptExercises01/DarkLightMode/main.js
@@ -5,3 +5,19 @@ const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+      darkMode();
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+      lightMode();
+    }
+  }
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
