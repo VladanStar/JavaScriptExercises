2b5009e31ba1d02fe9402ed681a6248f6cf3a2f3
unknown <vladancupric@gmail.com>
2022-08-09 11:48:28 +0200

    Add

--- JavaScriptExercises01/FormValidator/main.js
+++ JavaScriptExercises01/FormValidator/main.js
@@ -7,8 +7,23 @@ const message = document.getElementById("message");
 let isValid = false;
 let passwordsMatch = false;
 
+
+function validateForm() {
+    // Use HTML constraint API to check form validity
+    isValid = form.checkValidity();
+    // If the form isn't valid
+    if (!isValid) {
+      // Style main message for an error
+      message.textContent = 'Please fill out all fields.';
+      message.style.color = 'red';
+      messageContainer.style.borderColor = 'red';
+      return;
+    }
+}
 // Event Listener
 form.addEventListener("submit", processFormData);
+
+
 
 function processFormData(e) {
   e.preventDefault();
@@ -20,3 +35,4 @@ function processFormData(e) {
     storeFormData();
   }
 }
+
