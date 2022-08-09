ce1b67a2a1a4067caa98227643f4ea54e3b93c7e
unknown <vladancupric@gmail.com>
2022-08-09 11:27:51 +0200

    New commits

--- JavaScriptExercises01/FormValidator/main.js
+++ JavaScriptExercises01/FormValidator/main.js
@@ -1,5 +1,19 @@
-const form = document.getElementById('form');
-const password1El = document.getElementById('password1');
-const password2El = document.getElementById('password2');
-const messageContainer = document.querySelector('.message-container');
-const message = document.getElementById('message');
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
+
+// Event Listener
+form.addEventListener("submit", processFormData);
+
+function processFormData(e) {
+  e.preventDefault();
+  console.log(e);
+  // Validate Form
+  validateForm();
+  // Submit Form if Valid
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
+}
