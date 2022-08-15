0c31c674c26f806b357661bacead36e9dcddebb2
unknown <vladancupric@gmail.com>
2022-08-15 12:17:55 +0200

    New commits

--- ReactExercises/task06/taks06/src/App.js
+++ ReactExercises/task06/taks06/src/App.js
@@ -1,12 +1,31 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const title = "Blog Post";
+  const body = "This is my blog post";
+  const comments =[
+    {id:1, text:"Comment One"},
+    {id:2,text:"Comment Two"},
+    {id:3,text:"Comment Three"}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
+   
       <h1>Vladan Cupric</h1>
-      </header>
+      <h2>{title.toUpperCase()}</h2>
+      <p>{body}</p>
+      {Math.random}
+      <div className="commnet">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, id)=>(
+            <li>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    
     </div>
   );
 }
